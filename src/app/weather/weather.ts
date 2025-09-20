e90fa231b9fe6fb5fe36bf08e7e76193a83ad794
Salas Martinez <giovanni.salas-martinez.042@my.csun.edu>
2025-09-19 18:04:24 -0700

    Week 4

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 export class Weather {
+  forecasts: WeatherData[] = [];
+
   constructor(http: HttpClient) {
     http.get<WeatherData[]>('http://localhost:5153/weatherforecast').subscribe(result => {
       console.log(result)
+      this.forecasts = result;
     })
   }
 }
