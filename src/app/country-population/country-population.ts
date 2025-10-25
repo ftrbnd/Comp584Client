c658e993f19acd4331ecc998f32ac5c7c4caac3f
Salas Martinez <giovanni.salas-martinez.042@my.csun.edu>
2025-10-24 18:13:15 -0700

    Week 9

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountryPopulationData } from './country-population-data';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss',
 })
-export class CountryPopulation {
-  country: CountryPopulationData | null = null;
+export class CountryPopulation implements OnInit {
+  countryPopulation: CountryPopulationData | null = null;
 
-  constructor(http: HttpClient) {
-    http
-      .get<CountryPopulationData>(`${environment.apiUrl}/api/countries/25/population`)
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const countryId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.http
+      .get<CountryPopulationData>(`${environment.apiUrl}/api/countries/${countryId}/population`)
       .subscribe((result) => {
         console.log(result);
-        this.country = result;
+        this.countryPopulation = result;
       });
   }
 }
