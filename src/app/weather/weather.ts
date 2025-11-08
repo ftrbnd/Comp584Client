aa67069900d83bb7ede2b884294ffa82084c8f32
Salas Martinez <giovanni.salas-martinez.042@my.csun.edu>
2025-11-07 18:11:32 -0800

    Use async pipes

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -2,20 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Component, input } from '@angular/core';
 import { WeatherData } from '../weather-data';
 import { MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
-  imports: [MatTableModule],
+  imports: [MatTableModule, AsyncPipe],
   templateUrl: './weather.html',
   styleUrl: './weather.scss',
 })
 export class Weather {
-  forecasts: WeatherData[] = [];
+  forecasts$: Observable<WeatherData[]>;
 
   constructor(http: HttpClient) {
-    http.get<WeatherData[]>('http://localhost:5153/weatherforecast').subscribe((result) => {
-      console.log(result);
-      this.forecasts = result;
-    });
+    this.forecasts$ = http.get<WeatherData[]>('http://localhost:5153/weatherforecast');
   }
 }
