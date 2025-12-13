5c0fe26738001b27f49179f5ee5f3508b9e028b3
Salas Martinez <giovanni.salas-martinez.042@my.csun.edu>
2025-12-12 17:16:39 -0800

    Show cities

---
+++ src/app/city/city-data.ts
@@ -0,0 +1,8 @@
+export interface CityData {
+  id: number;
+  countryId: number;
+  name: string;
+  lat: number;
+  lng: number;
+  population: number;
+}
--- src/app/city/city.ts
+++ src/app/city/city.ts
@@ -1,11 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import { CityData } from './city-data';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-city',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './city.html',
-  styleUrl: './city.scss'
+  styleUrl: './city.scss',
 })
 export class City {
+  cities$: Observable<CityData[]>;
 
+  constructor(http: HttpClient) {
+    this.cities$ = http.get<CityData[]>(`${environment.apiUrl}/api/cities`);
+  }
 }
